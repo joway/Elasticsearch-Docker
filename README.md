f1f5e6eb51f3b35ba7e7cd5973b8321f68e4750e
joway <joway.w@gmail.com>
2016-09-05 21:46:10 +0800

    http auth disable

--- README.md
+++ README.md
@@ -2,3 +2,5 @@ ElasticSearch in Docker
 ======
 
 中文优化版ElasticSearch集成包
+
+http://localhost:9200/_plugin/rtf/
--- docker-entrypoint.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-groupadd elasticsearch
-useradd elasticsearch -g elasticsearch -p /usr/share/elasticsearch/
-chown -R elasticsearch:elasticsearch /usr/share/elasticsearch/
-su elasticsearch
